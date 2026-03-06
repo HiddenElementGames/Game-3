a6e4ffc45aec8bdc4fbaab362e7f0a4f45990c16
Keaton <kmiller3@uccs.edu>
2026-03-05 21:34:01 -0700

    Fixed block placement overlap with player
    Player can no longer place blocks if they are standing where the block would be. Prevents the player from launching themselves

--- Assets/Scripts/BlockPlacementSystem.cs
+++ Assets/Scripts/BlockPlacementSystem.cs
@@ -6,6 +6,9 @@ public class BlockPlacementSystem : MonoBehaviour
     [SerializeField] private Transform placementStartPoint;
     [SerializeField] private GameObject blockPrefab;
 
+    // used for checking if player is in the way of block placement. Value is roughly half the size of a block
+    private Vector3 halfExtents = new Vector3(0.49f, 0.49f, 0.49f);
+
     private void Update()
     {
         if(Mouse.current.rightButton.wasPressedThisFrame)
@@ -18,20 +21,16 @@ public class BlockPlacementSystem : MonoBehaviour
     {
         if(Physics.Raycast(placementStartPoint.position, placementStartPoint.forward, out RaycastHit hitInfo))
         {
-            if(hitInfo.collider.CompareTag("Block"))
+            // calculate the spawn position
+            Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x / 2), 
+                                                Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), 
+                                                Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2));
+
+			// ensure the player is not in the way before spawning the box
+			if (Physics.OverlapBox(spawnPosition, halfExtents).Length == 0)
             {
-                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x / 2), 
-                                                    Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), 
-                                                    Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2));
-                Instantiate(block, spawnPosition, Quaternion.identity);
-            }
-            else
-            {
-                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x), 
-                                                    Mathf.RoundToInt(hitInfo.point.y), 
-                                                    Mathf.RoundToInt(hitInfo.point.z));
-                Instantiate(block, spawnPosition, Quaternion.identity);
-            }
+				Instantiate(block, spawnPosition, Quaternion.identity);
+			}
         }
     }
 }
