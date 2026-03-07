2620c262b2c72770b61ca5d53b6b1c59620fbf60
Keaton <kmiller3@uccs.edu>
2026-03-06 17:38:21 -0700

    Added shop UI initialization

---
+++ Assets/Scripts/ShopItemSetupData.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItemSetupData : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI DisplayNameText;
+    [SerializeField] public Button PurchaseButton;
+    [SerializeField] public Image ItemImage;
+    [SerializeField] public TextMeshProUGUI ItemCostText;
+}
